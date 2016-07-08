ec6578b30a5ecb5c4dfd5a333911f3d0528cdf94
REX <rex@boom.local>
2016-07-08 10:05:04 +0800

    Initial Commit

---
+++ 相册访问----02/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  相册访问----02
+//
+//  Created by REX on 16/7/8.
+//  Copyright © 2016年 REX. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
